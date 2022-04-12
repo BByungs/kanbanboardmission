b62ef0a58a2abfddddc8af3e10b891febecc3d16
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-04-13 01:01:55 +0900

    feat: initial CardList Data 생성

---
+++ src/store/data.ts
@@ -0,0 +1,17 @@
+import { CardListType } from 'src/types/CardListType';
+
+export const CARDLIST: CardListType = {
+  todo: [
+    { id: 'asdsadsada', title: '카드 컴포넌트 만들기', content: '카드' },
+    {
+      id: 'safasfas!@%!%ASDasdasdas',
+      title: '드래그 이벤트 만들기',
+      content:
+        '드래그 드래그 드래그 드래그 드래그 드래그 드래그 드래그 드래그 asdsa asdsad sadasdas dasdasdsasdas asdsadsadasdassadasdsadsa',
+    },
+  ],
+  inProgress: [
+    { id: 'asdsadsad@!$a', title: '보드 컴포넌트 만들기', content: '보드' },
+  ],
+  done: [],
+};
