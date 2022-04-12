140fe3a7388d875a6d23fd12d63ff3f0d8724111
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-04-13 01:11:36 +0900

    feat: todo , inprogress , done 각각 3개의 Board 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import Board from './components/Board';
+import { StoreState } from './types/store';
+
 const App = (): JSX.Element => {
+  const cardListState = useSelector((state: StoreState) => state.cardList);
+  console.log(cardListState);
+
   return (
-    <div>
-      <div>Hello!</div>
-    </div>
+    <Container>
+      <Board progress="To do" list={cardListState.todo} />
+      <Board progress="In progress" list={cardListState.inProgress} />
+      <Board progress="Done" list={cardListState.done} />
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  margin: 0 auto;
+  max-width: 1040px;
+  padding: 20px;
+`;
 
 export default App;
