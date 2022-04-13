c3eda9324977e4e90fbadd09a28683b79ff1da69
안병진 <byungjin0120@gmail.com>
2022-04-13 18:39:12 +0900

    refactor: 플러스 버튼을 눌렀을때 AddCard 컴포넌트가 나오게 세팅

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -3,6 +3,8 @@ import { CardType } from 'src/types/CardType';
 import Empty from 'src/components/empty';
 import Card from 'src/components/Card';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import AddCard from '../AddCard';
 
 interface BoardProps {
   progress: string;
@@ -10,12 +12,18 @@ interface BoardProps {
 }
 
 const Board = (props: BoardProps): JSX.Element => {
+  const [addOpen, setAddOpen] = useState<boolean>(false);
+  const handleAdd = () => {
+    setAddOpen((prev) => !prev);
+  };
+
   return (
     <Container>
       <Header>
         <h1>{props.progress}</h1>
-        <PlusIcon icon={faPlus} />
+        <PlusIcon icon={faPlus} onClick={handleAdd} />
       </Header>
+      {addOpen && <AddCard setAddOpen={setAddOpen} progress={props.progress} />}
       {!props.list.length ? (
         <Empty />
       ) : (
