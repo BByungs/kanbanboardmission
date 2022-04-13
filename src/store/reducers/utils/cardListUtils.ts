cf60c6d6bd55724a4ca107f25e33e5479411d9bd
안병진 <byungjin0120@gmail.com>
2022-04-13 18:12:36 +0900

    refactor: cardPlus 기능 완성

--- src/store/reducers/utils/cardListUtils.ts
+++ src/store/reducers/utils/cardListUtils.ts
@@ -1,14 +1,31 @@
 import { PlusAndEditParams } from 'src/store/actions/cardList';
 import { CardListType } from 'src/types/CardListType';
 
-export const cardDelete = (cardListState: CardListType, id: string) => {};
+export const cardDelete = (cardListState: CardListType, id: string) => {
+  Object.keys(cardListState).forEach((idx: string) => {
+    const cards = cardListState[idx as keyof CardListType];
+    cards.forEach((card) => {
+      if (card.id === id) console.log(card);
+    });
+  });
+
+  return cardListState;
+};
 
 export const cardEdit = (
   cardListState: CardListType,
-  { id, title, content }: PlusAndEditParams,
+  { id, title, content, progress }: PlusAndEditParams,
 ) => {};
 
 export const cardPlus = (
   cardListState: CardListType,
-  { id, title, content }: PlusAndEditParams,
-) => {};
+  { id, title, content, progress }: PlusAndEditParams,
+) => {
+  const addData = cardListState[progress as keyof CardListType].unshift({
+    id,
+    title,
+    content,
+  });
+
+  return { ...cardListState, addData };
+};
