56713101dcfc2b837a86e9afe97b5f932dda78e2
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-04-14 02:51:24 +0900

    design: 스크롤바 안보이게 스타일링

--- src/components/Board/style.ts
+++ src/components/Board/style.ts
@@ -2,15 +2,20 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Container = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   width: 300px;
   height: 950px;
   max-height: 950px;
   background-color: #e8e9ed;
-  padding: 23px 19px 45px 19px;
+  padding: 23px 19px 20px 19px;
   margin: auto;
   border-radius: 8px;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export const Header = styled.div`
